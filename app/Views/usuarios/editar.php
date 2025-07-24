14aa896ffccc1b74956968a2aafee9d0794a670a
Masiel04 <adriana.carua7617@utc.edu.ec>
2025-07-24 05:26:11 -0500

    rutas

--- app/Views/usuarios/editar.php
+++ app/Views/usuarios/editar.php
@@ -1,7 +1,7 @@
 <?= $this->extend('plantilla') ?>
 <?= $this->section('contenido') ?>
 
-<h2>Editar Usuario</h2>
+<h2>Actualizar Usuario</h2>
 <form method="post" action="/usuario/actualizar/<?= $usuario['id_usuario'] ?>">
     <input type="text" name="nombre" value="<?= $usuario['nombre'] ?>" required><br>
     <input type="email" name="correo" value="<?= $usuario['correo'] ?>" required><br>
